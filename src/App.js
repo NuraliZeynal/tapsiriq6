798890c1d6456f772030da33c6987c16028a7a6b
nuralizeynal <nurelizeynalli@gmail.com>
2022-12-09 17:41:14 +0300

    salam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: ""
+  };
+
+  changeHandler = (e) => {
+    if (e.target.id === "name") {
+      this.setState({
+        name: /*document.querySelector("#name").value*/ e.target.value
+      });
+    }
+    if (e.target.id === "email") {
+      this.setState({
+        email: /*document.querySelector("#email").value*/ e.target.value
+      });
+    }
+    if (e.target.id === "password") {
+      this.setState({
+        password: /*document.querySelector("#password").value*/ e.target.value
+      });
+    }
+  };
+
+  clickHandler = (e) => {
+    if (e.target.id === "ad") {
+      this.setState({
+        name: ""
+      });
+    }
+    if (e.target.id === "mail") {
+      this.setState({
+        email: ""
+      });
+    }
+    if (e.target.id === "parol") {
+      this.setState({
+        password: ""
+      });
+    }
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <label htmlFor="name">
+          Name:
+          <input
+            id="name"
+            onChange={this.changeHandler}
+            value={this.state.name}
+          />
+        </label>
+        <button id="ad" onClick={this.clickHandler}>
+          x
+        </button>
+        <br />
+        <label htmlFor="email">
+          E-mail:
+          <input
+            id="email"
+            onChange={this.changeHandler}
+            value={this.state.email}
+          />
+        </label>
+        <button id="mail" onClick={this.clickHandler}>
+          x
+        </button>
+        <br />
+        <label htmlFor="password">
+          Password:
+          <input
+            id="password"
+            type="password"
+            onChange={this.changeHandler}
+            value={this.state.password}
+          />
+        </label>
+        <button id="parol" onClick={this.clickHandler}>
+          x
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
